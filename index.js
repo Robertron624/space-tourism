d60923965238375e374ca6e96cd42dc8516c90b9
Roberto Ramirez <robert.624@hotmail.com>
2023-07-09 16:15:57 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Carlos",
+    lastname: "Granada",
+    direccion_domicilio: "Carrera 80 Calle 16 Bucaramanga",
+    telefono: "312 123 4567",
+    nombre_acudiente: "Carlos Granada",
+    edad: 10,
+    pasatiempos: ["Jugar", "Leer", "Dibujar"],
+};
+
